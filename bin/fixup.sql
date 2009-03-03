61da24f9ce8a2268cbde1764d0fac89664e4de7a
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 02:35:43 -0800

    bin/fixup.sql: some untested changes.

--- bin/fixup.sql
+++ bin/fixup.sql
@@ -1,15 +1,14 @@
 delete from trips where service <> 1;
+delete from stops where not stop;
+alter table stops drop column stop;
 
-create view stop_codes as
+update stops set code='718' where code='R09';
+
+create temp view stop_codes as
 	select distinct code from stops where stop;
 
-create view real_stations as
+create temp view real_stations as
 	select * from stations natural join stop_codes
 	where x is not null and y is not null;
 
-update stops set code='718' where code='R09';
 delete from stations where code not in (select code from real_stations);
-drop view stop_codes cascade;
-
-delete from stops where not stop;
-alter table stops drop column stop;
