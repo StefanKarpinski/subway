3152004ba85964467d708e3e44f9837268f6477c
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 12:03:17 -0800

    bin/parse.sh: simplest way to only consider daily service (1).

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-perl bin/parse.pl rtif/*/rtif.*
+perl bin/parse.pl rtif/*/rtif.*..1.*
 cat bin/schema.sql | psql
 for x in stations trips stops; do
   cat data/$x.tab | psql -c "copy $x from stdin with null as 'NULL'"
