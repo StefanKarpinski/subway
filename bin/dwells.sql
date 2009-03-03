f25aadb04bf0420baf213cd8060de3ea38972f2d
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 02:49:24 -0800

    bin/{dwells,links}.sql: use route instead of ad-hoc route indicators.

--- bin/dwells.sql
+++ bin/dwells.sql
@@ -1,32 +1,22 @@
 drop table dwells, dwells_aggregate cascade;
 
-create temp view dwells_nonzero as select
-	trip,
-	trip_line,
-	direction,
-	orig_code,
-	dest_code,
-	code,
+create temp table dwells_nonzero as select
+	trip, route, code,
 	i.time as time_in,
 	o.time as time_out,
 	o.time - i.time as time
-from trips
+from trips natural join route_trips
 	join stops_indexed i using (trip)
 	join stops_indexed o using (trip,code)
 where
 	i.index + 1 = o.index;
 
 create temp view dwells_zero as select
-	trip,
-	trip_line,
-	direction,
-	orig_code,
-	dest_code,
-	code,
+	trip, route, code,
 	time as time_in,
 	time as time_out,
 	0 as time
-from trips
+from trips natural join route_trips
 	join stops using (trip);
 
 create table dwells as
@@ -34,25 +24,11 @@ create table dwells as
 	select * from dwells_zero;
 
 create table dwells_aggregate as select
-	trip_line,
-	direction,
-	orig_code,
-	dest_code,
-	code,
+	route, code,
 	avg(time) as avg_time,
 	stddev_samp(time) as std_time,
 	min(time) as min_time,
 	max(time) as max_time,
 	count(time) as count
-from dwells group by
-	trip_line,
-	direction,
-	dest_code,
-	orig_code,
-	code
-order by
-	trip_line,
-	direction,
-	dest_code,
-	orig_code,
-	code;
+from dwells group by route, code
+order by route, code;
--- bin/links.sql
+++ bin/links.sql
@@ -1,17 +1,13 @@
 drop table links, links_aggregate cascade;
 
 create table links as select
-	trip,
-	trip_line,
-	direction,
-	orig_code,
-	dest_code,
+	trip, route,
 	o.code as code_out,
 	i.code as code_in,
 	o.time as time_out,
 	i.time as time_in,
 	i.time - o.time as time
-from trips
+from trips natural join route_trips
 	join stops_indexed o using (trip)
 	join stops_indexed i using (trip)
 where
@@ -19,28 +15,12 @@ where
 	o.index + 1 = i.index;
 
 create table links_aggregate as select
-	trip_line,
-	direction,
-	orig_code,
-	dest_code,
-	code_out,
-	code_in,
+	route, code_out, code_in,
 	avg(time) as avg_time,
 	stddev_samp(time) as std_time,
 	min(time) as min_time,
 	max(time) as max_time,
 	count(time) as count
-from links group by
-	trip_line,
-	direction,
-	dest_code,
-	orig_code,
-	code_out,
-	code_in
-order by
-	trip_line,
-	direction,
-	dest_code,
-	orig_code,
-	code_out,
-	code_in;
+from links
+group by route, code_out, code_in
+order by route, code_out, code_in;
