c838d71386899c74073f3de03773c2db3cd95b78
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 02:14:13 -0800

    bin/routes.sql: compute distinct routes based on direction & stops.

---
+++ bin/routes.sql
@@ -0,0 +1,37 @@
+drop table routes, route_trips, route_codes cascade;
+
+create temp view trip_codes as
+	select distinct trip, code
+	from stops order by trip, code;
+
+create temp view trip_route_stops as
+	select trip, array_to_string(array_accum(code),',') as route_stops
+	from trip_codes group by trip;
+
+create temp view trip_dir_routes as
+	select trip, direction, route_stops
+	from trips natural join trip_route_stops;
+
+create temp sequence route_index_seq;
+create temp table routes_tmp as select
+	nextval('route_index_seq') as route,
+	direction,
+	count(*),
+	route_stops
+from trip_dir_routes
+group by route_stops, direction
+order by route_stops, direction;
+
+create table route_trips as
+	select route, trip
+	from trip_route_stops natural join routes_tmp
+	order by route, trip;
+
+create table route_codes as
+	select distinct route, code
+	from route_trips natural join trip_codes
+	order by route, code;
+
+create table routes as
+	select route, direction, count
+	from routes_tmp;
