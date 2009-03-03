a7aa99669452849b6965ed7654269531fedccd85
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 15:22:30 -0800

    bin/fixup.sql: simplify pruning invalid stations.

--- bin/fixup.sql
+++ bin/fixup.sql
@@ -5,9 +5,8 @@ create index stops_type_idx on stops (type);
 
 update stops set code='718' where code='R09';
 
-create temp view codes as select distinct code from stops;
-create temp view used_stations as
-	select * from stations natural join codes
+create temp view used_codes as select distinct code
+	from stops natural join stations
 	where x is not null and y is not null;
 
-delete from stations where code not in (select code from used_stations);
+delete from stations where code not in (select code from used_codes);
