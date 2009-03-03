26365960534bddd93ede3d7f8421fb60f1963452
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 02:42:48 -0800

    bin/index.sql: add index index on stops_indexed.

--- bin/index.sql
+++ bin/index.sql
@@ -4,3 +4,5 @@ create temp sequence stops_index_seq;
 create table stops_indexed as
 	select nextval('stops_index_seq') as index, *
 	from stops order by trip, time;
+
+create unique index stops_index_idx on stops_indexed (index);
