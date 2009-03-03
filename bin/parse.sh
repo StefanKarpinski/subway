4233519d57690c59bc48485ccc791b443283b307
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-02 21:39:51 -0800

    Shell script to do RTIF parsing and insertion into database.

---
+++ bin/parse.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+perl bin/parse.pl rtif/*/rtif.*
+cat bin/schema.sql | psql
+for x in stations trips stops; do
+  cat data/$x.tab | psql -c "copy $x from stdin with null as 'NULL'"
+done
