ce6b6d5ed5ee34f22bef973c44f28de892adb754
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-03-03 02:41:46 -0800

    bin/fixup.sql: add indexes on stops table for speed.

--- bin/fixup.sql
+++ bin/fixup.sql
@@ -2,6 +2,10 @@ delete from trips where service <> 1;
 delete from stops where not stop;
 alter table stops drop column stop;
 
+create index stops_trip_idx on stops (trip);
+create index stops_code_idx on stops (code);
+create index stops_time_idx on stops (time);
+
 update stops set code='718' where code='R09';
 
 create temp view stop_codes as
